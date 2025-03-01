b777469a8f56e71241e910f254df32a9f6fb17f8
nicolomaiorano <maiorano.nicolo@gmail.com>
2025-03-01 13:14:56 +0100

    Inserita scelta completamente random della domanda

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from contextlib import nullcontext
+import random
 
 domande = open("domande.txt", "r")
 punti = open("punti.txt", "r")
 livelloMax = 0
-diff = 0
 
 class domanda:
     def __init__(self, testo, difficolta, risposta, rispostaS1, rispostaS2, rispostaS3):
@@ -30,7 +29,7 @@ for x in l:
     if x == '':
         d = domanda(b[0], b[1], b[2], b[3], b[4], b[5])
         if int(b[1]) > int(livelloMax):
-            livelloMax = b[1]
+            livelloMax = int(b[1])
         listaDomande.append(d)
         i=0
         b = ['','','','','','']
@@ -38,31 +37,55 @@ for x in l:
         b[i] = x
         i = i+1
 
+lista = [None] * (livelloMax+1)
+
+i=0
+
+for k in range(livelloMax+1):
+    lista[i] = 0
+    i = i + 1
+
+i=0
+
+while i < int(livelloMax+1):
+    for d in listaDomande:
+        if int(d.difficolta) == int(i):
+            lista[i] = int(lista[i]) + 1
+    i = i+1
+
 errore = 0
 print("Difficoltà massima raggiungibile: ", livelloMax)
+
 punteggio = 0
+diff = 0
+c = 1
 
 while errore == 0 and diff <= int(livelloMax):
     print("Difficoltà ", diff, ":")
+    randomNum = random.randint(1, lista[diff])
+
     for d in listaDomande:
         if int(d.difficolta) == diff:
-            print(d.testo)
-            print(d.risposta)
-            print(d.rispostaS1)
-            print(d.rispostaS2)
-            print(d.rispostaS3)
+            if c == randomNum:
+                print(d.testo)
+                print(d.risposta)
+                print(d.rispostaS1)
+                print(d.rispostaS2)
+                print(d.rispostaS3)
 
-            inserimento = input("Inserire la risposta: ")
-            giusto = d.risposta
-            break
+                inserimento = input("Inserire la risposta: ")
 
-    if inserimento == giusto:
-        diff = diff + 1
-        print("Complimenti, risposta esatta!")
-        punteggio = punteggio +1
-    else:
-        print("Hai sbagliato, fine del gioco!")
-        errore = 1
+                c=1
+                if inserimento == d.risposta:
+                    diff = diff + 1
+                    print("Complimenti, risposta esatta!")
+                    punteggio = punteggio + 1
+                else:
+                    print("Hai sbagliato, fine del gioco!")
+                    errore = 1
+                break
+            else:
+                c = c+1
 
 print("Il tuo punteggio finale è: ", punteggio)
 nick = input("Inserisci il tuo nickname: ")
